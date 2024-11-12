29b2a50bfc90a264a4a864ae724420817fed455a
aswiniiiiiiiii <aswinips787@gmail.com>
2024-11-12 20:30:56 +0530

    Node-DB-Exam

--- routes/router.js
+++ routes/router.js
@@ -5,13 +5,17 @@ const jwtMiddleware = require('../middlewares/jwtMiddleware')
 const  router = new express.Router()
 
 
-//user registration to http://localhost:3000/user-register
+//user registration to http://localhost:3001/user-register
 router.post('/user-register',usercontroller.userRegistercontroller)
 
-//login to http://localhost:3000/login
+//login to http://localhost:3001/login
+router.post('/login',usercontroller.loginController)
+
+// one-users :http://localhost:3001/one-user
+router.get('/one-user',jwtMiddleware,usercontroller.oneUserController)
+
+// all-users : http://localhost:3001/all-users
 router.get('/all-users',jwtMiddleware,usercontroller.allUserController)
 
-// one-users
-router.get('/one-user',jwtMiddleware,usercontroller.oneUserController)
 
 module.exports = router
